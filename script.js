8fc1221d3d0384e3fb766320a9bb5749c3d2abd9
Stephenwang3801 <stephenwang3801@gmail.com>
2021-09-20 15:12:29 -0400

    Added panning on map.
    Map has panning animation to workouts whenever the workout is clicked.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + "").slice(-10);
-
+  //clicks = 0;
   constructor(coords, distance, duration) {
     this.coords = coords;
     this.distance = distance;
@@ -16,6 +16,10 @@ class Workout {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
   }
+
+  //click() {
+  //  this.clicks++;
+  //}
 }
 
 class Running extends Workout {
@@ -66,6 +70,7 @@ class App {
     this._getPosition();
     form.addEventListener("submit", this._newWorkout.bind(this));
     inputType.addEventListener("change", this._toggleElevationField);
+    containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -232,6 +237,29 @@ class App {
       `;
     }
     form.insertAdjacentHTML("afterend", html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest(".workout");
+    console.log(workoutEl);
+
+    if (!workoutEl) {
+      return;
+    }
+
+    const workout = this.#workouts.find(
+      (work) => work.id === workoutEl.dataset.id
+    );
+
+    console.log(workout);
+
+    this.#map.setView(workout.coords, 13, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    //workout.click();
   }
 }
 
