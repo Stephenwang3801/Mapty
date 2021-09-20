cb63a5290462ef1e770d4cf50619d9059df8ed70
Stephenwang3801 <stephenwang3801@gmail.com>
2021-09-20 15:44:45 -0400

    Added Local Storage
    Workouts can now be saved and reopened through reloading the webpage.

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ class App {
 
   constructor() {
     this._getPosition();
+    this._getLocalStorage();
     form.addEventListener("submit", this._newWorkout.bind(this));
     inputType.addEventListener("change", this._toggleElevationField);
     containerWorkouts.addEventListener("click", this._moveToPopup.bind(this));
@@ -97,6 +98,10 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on("click", this._showForm.bind(this));
+
+    this.#workouts.forEach((work) => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -168,6 +173,8 @@ class App {
     //Hide form and clear input fields
     this._hideForm();
 
+    //Local storage for workouts
+    this._setLocalStorage();
     //Clearing Input fields
   }
 
@@ -260,6 +267,25 @@ class App {
       },
     });
     //workout.click();
+  }
+  _setLocalStorage() {
+    localStorage.setItem("workouts", JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem("workout"));
+    if (!data) {
+      return;
+    }
+    this.#workouts = data;
+
+    this.#workouts.forEach((work) => {
+      this._renderWorkout(work);
+    });
+  }
+  reset() {
+    localStorage.removeItem("workouts");
+    location.reload();
   }
 }
 
